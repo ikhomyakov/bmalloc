9c54df5c963cf28fc18d75ac89c3336299db3df4
Igor Khomyakov <ikh@awaresw.com>
2001-03-20 05:39:17 +0000

    first draft

--- bmalloc.c
+++ bmalloc.c
@@ -1,145 +1,151 @@
-/*
- * Copyright (c) 2001 by W-Technologies, Inc.
- * All Rights Reserved.
- */
-
-Entry points  for  malloc_debug(),  mallocmap(),  mallopt(),
-     mallinfo(),  memalign(),  and  valloc(), are empty routines,
-     and are provided only to protect the user from  mixing  mal-
-     loc() functions from different implementations.
-
-#ifdef _REENTRANT
-#endif
-
-#ifndef _SIZE_T
-#define _SIZE_T
-typedef unsigned int    size_t;
-#endif
-
-#ifndef NULL
-#define NULL    0
-#endif
-
-#include <stdlib.h>
-extern void *calloc(size_t, size_t);
-extern void free(void *);
-extern void *malloc(size_t);
-extern void *realloc(void *, size_t);
-
-extern void *valloc(size_t);
-
-extern void *memalign(size_t, size_t);
-
-
-void *malloc(size_t size);
-void *calloc(size_t nelem, size_t elsize);
-void free(void *ptr);
-void *memalign(size_t alignment, size_t size);
-void *realloc(void *ptr, size_t size);
-void *valloc(size_t size);
-
-#include <alloca.h>
-void *alloca(size_t size);
-
-#include <malloc.h>
-int mallopt(int cmd, int value);
-struct mallinfo mallinfo(void);
-
-
-/* -------- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <malloc.h>
 
-#define getpagesize() sysconf(_SC_PAGE_SIZE)
+#define getpagesize() sysconf(_SC_PAGESIZE)
+#define	MAGIC 0xef
+#define	NBUCKETS 30
+
 
 union overhead {
-  union	overhead *ov_next;	/* when free */
+  union	overhead *next;
   struct {
-    unsigned char	ovu_magic; /* magic number */
-    unsigned char	ovu_index; /* bucket # */
-  } ovu;
-#define	ov_magic	ovu.ovu_magic
-#define	ov_index	ovu.ovu_index
+    unsigned char magic;
+    unsigned char bucket;
+  } ov;
 };
 
-#define	MAGIC		0xef		/* magic # on accounting info */
+struct heap {
+  union overhead *nextf[NBUCKETS];
+  unsigned int nmalloc[NBUCKETS];
+  unsigned int nfree[NBUCKETS];
+};
 
-#define	NBUCKETS 30
-static	union overhead *nextf[NBUCKETS];
-
-static	int pagesz;			/* page size */
-static	int pagebucket;			/* page size bucket */
-
-static  void dumpstats( void );
-
-#ifdef MSTATS
-static	unsigned int nmalloc[NBUCKETS];
-static  void mstats( char *s );
-#endif
-
+static void dumpstats( void );
+static void mstats( char *s );
 static void morecore(int bucket);
+static int  init( void );
+static struct heap *gethp( void );
+static void key_destructor(void *value);
 
-void * malloc(size_t nbytes)
+static size_t pagesz;
+static int    pagebucket;
+static pthread_key_t   key;
+
+static int init( void )
 {
-  union overhead *op;
-  int bucket, n;
-  unsigned amt;
+  static pthread_mutex_t initlock;
+  int bucket
+  size_t n, amt;
 
-  if (pagesz == 0) {
-    pagesz = getpagesize();
-    n = pagesz;
-    op = (union overhead *)sbrk(0);
-    n = n - sizeof (*op) - ((int)op & (n - 1));
-    if (n < 0)
-      n += pagesz;
-    if (n) {
-      if (sbrk(n) == (char *)-1)
-	return (NULL);
-    }
-    bucket = 0;
-    amt = 8;
-    while (pagesz > amt) {
+  pthread_mutex_lock(&initlock);
+  if( pagesz != 0 ) {
+      pthread_mutex_unlock(&initlock);
+      return 0;
+  }
+
+  pagesz = getpagesize();
+  n = pagesz;
+  op = (union overhead *)sbrk(0);
+  n = n - sizeof(*op) - ((int)op & (n - 1));
+  if (n < 0 ) {
+    n += pagesz;
+  }
+  if( n ) {
+      if( (void *)sbrk(n) == (void *)-1 ) {
+          pagesz = 0;
+          pthread_mutex_unlock(&initlock);
+          return( -1 );
+      }
+  }
+  bucket = 0;
+  amt = 8;
+  while( pagesz > amt ) {
       amt <<= 1;
       bucket++;
-    }
-    pagebucket = bucket;
-    atexit(dumpstats);
   }
+  pagebucket = bucket;
+  /*atexit(dumpstats);*/
 
-  if (nbytes <= (n = pagesz - sizeof (*op))) {
-    amt = 8;
-    bucket = 0;
-    n = -(sizeof (*op) );
-  } else {
-    amt = pagesz;
-    bucket = pagebucket;
-  }
-  while (nbytes > amt + n) {
-    amt <<= 1;
-    if (amt == 0)
-      return (NULL);
-    bucket++;
-  }
+  pthread_key_create(&key, heap_dtor);
 
-  if ((op = nextf[bucket]) == NULL) {
-    morecore(bucket);
-    if ((op = nextf[bucket]) == NULL)
-      return (NULL);
-  }
-
-  nextf[bucket] = op->ov_next;
-  op->ov_magic = MAGIC;
-  op->ov_index = bucket;
-#ifdef MSTATS
-  nmalloc[bucket]++;
-#endif
-  return ((char *)(op + 1));
+  pthread_mutex_unlock(&initlock);
+  return( 0 );
 }
 
-static void morecore(int bucket)
+static void heap_dtor(void *value)
+{
+  /* dump stats */
+}
+
+static struct heap *gethp( void )
+{
+  struct heap *hp = NULL;
+  hp = (struct heap *)pthread_getspecific(key);
+  if( hp == NULL ) {
+      hp = (struct heap *)sbrk(pagesz);
+      if( (void *)hp == (void *)-1 ) {
+          return( NULL );
+      }
+      pthread_setspecific(key, (void *)hd );
+  }
+  return( hp );
+}
+
+void *malloc( size_t nbytes)
+{
+  struct heap *hp;
+  union overhead *op;
+  int bucket
+  size_t n, amt;
+
+  if( pagesz == 0 ) {
+      if( init() < 0 ) {
+          return( NULL );
+      }
+  }
+
+  hp = gethp();
+  if( hp == NULL ) {
+      return( NULL );
+  }
+  
+  n = pagesz - sizeof(*op);
+  if( nbytes <= n ) {
+      amt = 8;
+      bucket = 0;
+      n = -sizeof(*op);
+  } else {
+      amt = pagesz;
+      bucket = pagebucket;
+  }
+
+  while( nbytes > amt+n ) {
+      amt <<= 1;
+      if( amt == 0 ) {
+          return (NULL);
+      }
+      bucket++;
+  }
+
+  if( (op=hp->nextf[bucket]) == NULL ) {
+      morecore(hp,bucket);
+      if( (op=hp->nextf[bucket]) == NULL ) {
+          return (NULL);
+      }
+  }
+
+  hp->nextf[bucket] = op->ov.next;
+  op->ov.magic  = MAGIC;
+  op->ov.bucket  = bucket;
+  hp->nmalloc[bucket]++;
+  return ((void *)(op + 1));
+}
+
+static void morecore(struct heap *hp, int bucket)
 {
   union overhead *op;
   int sz;
@@ -157,116 +163,162 @@ static void morecore(int bucket)
     nblks = 1;
   }
   op = (union overhead *)sbrk(amt);
-  if ((int)op == -1)
+  if ((void *)op == (void *)-1)
     return;
 
-  nextf[bucket] = op;
+  hp->nextf[bucket] = op;
   while (--nblks > 0) {
-    op->ov_next = (union overhead *)((char *)op + sz);
+    op->ov.next = (union overhead *)((char *)op + sz);
     op = (union overhead *)((char *)op + sz);
   }
-  op->ov_next = 0;
+  op->ov.next = 0;
 }
 
 void free(void *cp)
 {   
-  int size;
+  struct heap *hp;
   union overhead *op;
+  int bucket;
 
   if (cp == NULL)
     return;
   op = (union overhead *)((char *)cp - sizeof (union overhead));
-  if (op->ov_magic != MAGIC)
+  if (op->ov.magic != MAGIC)
     return;
 
-  size = op->ov_index;
-  op->ov_next = nextf[size];
-  nextf[size] = op;
-#ifdef MSTATS
-  nmalloc[size]--;
-#endif
-}
+  hp = gethp();
+  if( hp == NULL ) {
+      return( NULL );
+  }
 
-void * realloc(void *cp, size_t nbytes)
+  bucket = op->ov.bucket;
+  op->ov.next = hp->nextf[bucket];
+  hp->nextf[bucket] = op;
+  hp->nfree[bucket]++;
+}
+ 
+void *realloc(void *cp, size_t nbytes)
 {   
-  unsigned int onb;
-  int i;
   union overhead *op;
-  char *res;
+  size_t onb, sz;
+  int bucket;
+  void *res;
   int was_alloced = 0;
 
-  if (cp == NULL)
-    return (malloc(nbytes));
-  op = (union overhead *)((char *)cp - sizeof (union overhead));
-  if (op->ov_magic == MAGIC)
-  {
-    was_alloced++;
-    i = op->ov_index;
+  if (cp == NULL) {
+      return( malloc(nbytes) );
   }
-  else
-  {
-    return 0;
-  }
-  onb = 1 << (i + 3);
-  if (onb < pagesz)
-    onb -= sizeof (*op);
-  else
-    onb += pagesz - sizeof (*op);
 
-  if (was_alloced) {
-    if (i) {
-      i = 1 << (i + 2);
-      if (i < pagesz)
-	i -= sizeof (*op);
-      else
-	i += pagesz - sizeof (*op);
-    }
-    if (nbytes <= onb && nbytes > i) {
-      return(cp);
-    }
-    else
-      test_free(cp);
+  op = (union overhead *)( (char *)cp - sizeof(union overhead) );
+
+  if( op->ov.magic == MAGIC ) {
+      was_alloced++;
+      bucket = op->ov.bucket;
+  } else {
+      return( NULL );
   }
-  if ((res = malloc(nbytes)) == NULL)
-    return (NULL);
-  if (cp != res)
-    memcpy(res, cp, (nbytes < onb) ? nbytes : onb);
-  return (res);
+
+  onb = 1 << (bucket + 3);
+  if( onb < pagesz ) {
+      onb -= sizeof(*op);
+  } else {
+      onb += pagesz - sizeof(*op);
+  }
+
+  if( was_alloced ) {
+    if( bucket ) {
+        sz = 1 << (bucket + 2);
+        if( sz < pagesz ) {
+            sz -= sizeof(*op);
+        } else {
+	    sz += pagesz - sizeof (*op);
+        }
+    }
+    if (nbytes <= onb && nbytes > sz) {
+        return( cp );
+    } else {
+      free( cp );
+  }
+
+  if( (res=malloc(nbytes)) == NULL ) {
+    return( NULL );
+  }
+
+  if( cp != res ) {
+    memcpy( res, cp, (nbytes<onb)?nbytes:onb );
+  }
+
+  return( res );
 }
 
-static void dumpstats( void )
+static void dumpstats( struct heap *hp )
 {
   int i, count;
-  char *rv;
-  for (i=0; i<NBUCKETS; i++) {
-    count=0;
-    for (rv=nextf[i]; rv; rv=*(char **)rv) count++;
-    if (count)
-      fprintf(stderr, "%2d  %12d  %12d\n", i, count, (1<<(i+3))*count);
+  union overhead *rv;
+  for( i=0; i!=NBUCKETS; i++ ) {
+      count=0;
+      for( rv=hp->nextf[i]; rv; rv=rv->next ) {
+          count++;
+      }
+      if( count )
+          fprintf( stderr, "t%u b%2d c%12d ~s%12d\n", 
+                   (unsigned int)pthread_self(), i, count, (1<<(i+3))*count
+          );
   }
 }
 
-#ifdef MSTATS
 static void mstats(char *s)
 {
-  	int i, j;
-  	union overhead *p;
-  	int totfree = 0,
-  	totused = 0;
+  int i, j;
+  union overhead *p;
+  int totfree = 0,
+  totused = 0;
 
-  	fprintf(stderr, "Memory allocation statistics %s\nfree:\t", s);
-  	for (i = 0; i < NBUCKETS; i++) {
-  		for (j = 0, p = nextf[i]; p; p = p->ov_next, j++)
-  			;
-  		fprintf(stderr, " %d", j);
-  		totfree += j * (1 << (i + 3));
-  	}
-  	fprintf(stderr, "\nused:\t");
-  	for (i = 0; i < NBUCKETS; i++) {
-  		fprintf(stderr, " %d", nmalloc[i]);
-  		totused += nmalloc[i] * (1 << (i + 3));
-  	}
-  	fprintf(stderr, "\n\tTotal in use: %d, total free: %d\n",
+  fprintf( stderr, "Memory allocation statistics %s %u\nfree:\t", 
+           s, (unsigned int)pthread_self() );
+
+  for( i=0; i!=NBUCKETS; i++) {
+      for( j=0,p=hp->nextf[i]; p; p=p->next,j++) {
+      }
+      fprintf( stderr, " %d", j );
+      totfree += j * (1 << (i + 3));
+  }
+  fprintf( stderr, "\nused:\t" );
+  for( i=0; i!=NBUCKETS; i++) {
+      fprintf(stderr, " %d", nmalloc[i]);
+      totused += (hp->nmalloc[i]-hp->nfree[i]) * (1 << (i + 3));
+  }
+  fprintf(stderr, "\n\tTotal in use: %d, total free: %d\n",
 	    totused, totfree);
 }
-#endif
+
+void *calloc(size_t nelem, size_t elsize)
+{
+  return( NULL );
+}
+
+void *memalign(size_t alignment, size_t size)
+{
+  return( NULL ); /* not supported */
+}
+
+void *valloc(size_t size)
+{
+  if( pagesz == 0 ) {
+      if( init() < 0 ) {
+          return( NULL );
+      }
+  }
+  return( memalign(pagesz,size) );
+}
+
+int mallopt(int cmd, int value)
+{
+  return(1); /* not supported */
+}
+
+struct mallinfo mallinfo(void)
+{
+  static struct mallinfo ret;
+  return( ret ); /* not supported */
+}
