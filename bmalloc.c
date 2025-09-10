87ee719bcf28271fa7866aae3e1180a909fd0dcb
Igor Khomyakov <ikh@ikhsoftware.com>
2025-09-09 23:26:45 -0600

    Released the software to open source under LGPL-3.0-or-later

--- bmalloc.c
+++ bmalloc.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 1995-2025 IKH Software, Inc. <support@ikhsoftware.com>
+ *
+ * This is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the software. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
